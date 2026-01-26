fa9c9067cb354be82f1a7f7f2be558c295c5577f
Josef Kolář <thejoeejoee@gmail.com>
2026-01-26 09:45:07 +0100

    admin wip

---
+++ app/components/admin/Main.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { IconRefreshDot } from '@tabler/icons-vue'
+</script>
+
+<template>
+  foo
+</template>
---
+++ app/pages/admin.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import type {NavigationMenuItem, TabsItem} from '@nuxt/ui'
+
+const items: TabsItem[] = [
+  {
+    label: 'All',
+    value: 'all'
+  },
+  {
+    label: 'Unread',
+    value: 'unread'
+  }
+]
+
+const menu: NavigationMenuItem[] = [{
+  label: 'Home',
+  icon: 'i-lucide-house',
+  active: true
+}, {
+  label: 'Inbox',
+  icon: 'i-lucide-inbox',
+  badge: '4'
+}, {
+  label: 'Contacts',
+  icon: 'i-lucide-users'
+}, {
+  label: 'Settings',
+  icon: 'i-lucide-settings',
+  defaultOpen: true,
+  children: [{
+    label: 'General'
+  }, {
+    label: 'Members'
+  }, {
+    label: 'Notifications'
+  }]
+}]
+</script>
+
+
+<template>
+  <UApp>
+    <UDashboardGroup>
+      <UDashboardSidebar collapsible>
+        <template #header>
+          <UDashboardSidebarCollapse/>
+        </template>
+
+        <template #default="{ collapsed }">
+          <UNavigationMenu
+              :collapsed="collapsed"
+              :items="menu"
+              orientation="vertical"
+          />
+        </template>
+      </UDashboardSidebar>
+
+      <UDashboardPanel>
+        <template #header>
+          <UDashboardNavbar title="Inbox">
+            <template #leading>
+
+            </template>
+
+            <template #trailing>
+              <UBadge label="4" variant="subtle"/>
+            </template>
+
+            <template #right>
+              <UTabs :items="items" default-value="all" size="sm" class="w-40" :content="false"/>
+            </template>
+
+
+          </UDashboardNavbar>
+          navbar
+        </template>
+      </UDashboardPanel>
+
+      <slot/>
+    </UDashboardGroup>
+
+  </UApp>
+</template>
