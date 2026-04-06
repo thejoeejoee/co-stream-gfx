e5215fcb65d3dc71a8b43f59dd29b379e5c24ec9
Josef Kolář <thejoeejoee@gmail.com>
2026-04-06 09:42:29 +0200

    fix: reconnect SSE

--- app/state.ts
+++ app/state.ts
@@ -3,7 +3,12 @@ import { useEventSource } from '@vueuse/core'
 export const params = new URLSearchParams(window.location.search)
 export const {
   eventSource
-} = useEventSource(params.get('sse') || 'http://localhost:8080/_sse/default')
+} = useEventSource(params.get('sse') || 'http://localhost:8080/_sse/default', [], {
+  autoReconnect: {
+    retries: -1,
+    delay: 1000,
+  }
+})
 
 // director alerts settings
 export const timeOffset = 0
